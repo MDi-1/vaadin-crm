f736bdaff32ba76b284eb870e76ae70459894903
MDi <calendarzyk@gmail.com>
2021-08-25 19:17:28 +0200

    final commit, so far

---
+++ src/main/java/com/vaadin/tutorial/crm/ui/ContactForm.java
@@ -0,0 +1,107 @@
+package com.vaadin.tutorial.crm.ui;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
+import com.vaadin.tutorial.crm.backend.Company;
+import com.vaadin.tutorial.crm.backend.Contact;
+
+import java.util.List;
+
+public class ContactForm extends FormLayout {
+
+    private Contact contact;
+    TextField firstName = new TextField("First name");
+    TextField lastName = new TextField("Last name");
+    EmailField email = new EmailField("Email");
+    ComboBox<Contact.Status> status = new ComboBox<>("Status");
+    ComboBox<Company> company = new ComboBox<>("Company");
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+
+    Button saveBtn = new Button("Save");
+    Button deleteBtn = new Button("Delete");
+    Button closeBtn = new Button("Cancel");
+
+    public ContactForm(List<Company> companies) {
+        addClassName("contact-form");
+        binder.bindInstanceFields(this);
+
+        company.setItems(companies);
+        company.setItemLabelGenerator(Company::getName);
+        status.setItems(Contact.Status.values());
+        add(firstName, lastName, email, company, status, createButtonsLayout());
+    }
+
+    private HorizontalLayout createButtonsLayout() {
+        saveBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        deleteBtn.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        closeBtn.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        saveBtn.addClickShortcut(Key.ENTER);
+        closeBtn.addClickShortcut(Key.ESCAPE);
+
+        saveBtn.addClickListener(event -> validateAndSave());
+        deleteBtn.addClickListener(event -> fireEvent(new DeleteEvent(this, contact)));
+        closeBtn.addClickListener(event -> fireEvent(new CloseEvent(this)));
+        binder.addStatusChangeListener(e -> saveBtn.setEnabled(binder.isValid()));
+        return new HorizontalLayout(saveBtn, deleteBtn, closeBtn);
+    }
+
+    private void validateAndSave() {
+        try {
+            binder.writeBean(contact);
+            fireEvent(new SaveEvent(this, contact));
+        } catch (ValidationException e) { e.printStackTrace(); }
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+        binder.readBean(contact);
+    }
+
+    // Events
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+        private Contact contact;
+
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+
+        public Contact getContact() {
+            return contact;
+        }
+    }
+
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,ComponentEventListener<T>listener){
+        return getEventBus().addListener(eventType, listener);
+    }
+}
